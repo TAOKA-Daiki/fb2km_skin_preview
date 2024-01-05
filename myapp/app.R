4876890bfb3b08abfc0f7efce00cb8d98e61c8b5
TAOKA-Daiki <taoka.daiki.777@gmail.com>
2024-01-06 01:00:51 +0900

    changed title

--- myapp/app.R
+++ myapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("fb2km skin preview"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
